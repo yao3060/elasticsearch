c964fecb15dfa19ba3ee7a428fb3f44cfcfb9d84
YangShangPu <yangshangpu@tuguaishou.com>
2021-09-02 18:03:22 +0800

    增加redis

---
+++ src/models/ES/Asset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\models\ES;
+
+use app\interfaces\ES\QueryBuilderInterface;
+
+class Asset extends BaseModel
+{
+    public function search(QueryBuilderInterface $query): array
+    {
+
+    }
+}
