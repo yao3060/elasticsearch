481ad9f0122a23b0bf6d4ed1bf7c57f941440205
YYY RMBP <you3060@163.com>
2021-09-09 13:48:07 +0800

    add UNIT & INTEGRATION TESTS description

--- src/tests/unit/_bootstrap.php
+++ src/tests/unit/_bootstrap.php
@@ -1,3 +1,20 @@
 <?php
 
+/**
+ * UNIT & INTEGRATION TESTS
+ * @see https://codeception.com/docs/05-UnitTests
+ *
+ * 1. 新建一个测试
+ * **********
+ * ./codecept generate:test unit models/SearchSvg
+ * 注意修改 `models/SearchSvgTest.php` 的 `namespace` 为 `tests\unit\models`
+ *
+ * 2. 执行测试
+ * **********
+ * ./codecept run unit models/SearchSvgTest
+ *
+ * 3. 执行所有单元测试
+ * ./codecept run unit
+ */
+
 // add unit testing specific bootstrap code here
