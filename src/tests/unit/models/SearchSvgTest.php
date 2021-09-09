f0ba0d097159d6bdfb933e81a3f091ea0178b8f7
YYY RMBP <you3060@163.com>
2021-09-09 14:07:49 +0800

    add testSearchSVGHeart

--- src/tests/unit/models/SearchSvgTest.php
+++ src/tests/unit/models/SearchSvgTest.php
@@ -54,4 +54,29 @@ class SearchSvgTest extends \Codeception\Test\Unit
 
         $this->assertEquals(join(',', $ids), join(',', $myIds));
     }
+
+    public function testSearchSVGHeart()
+    {
+        $items = (new Svg)->search(new SvgSearchQuery(
+            keyword: '心',
+            page: 1,
+            kid2: [],
+            pageSize: 50
+        ));
+
+        /**@var \GuzzleHttp\Psr7\Response $response */
+        $response = $this->http->request(
+            'GET',
+            'https://818ps.com/apiv2/search-asset-svg?p=1&k2=0&word=%E5%BF%83&pageSize=50'
+        );
+
+        $content = json_decode($response->getBody()->getContents());
+        $ids = ArrayHelper::getColumn($content->msg, 'id');
+        sort($ids);
+
+        $myIds = $items['ids'];
+        sort($myIds);
+
+        $this->assertEquals(join(',', $ids), join(',', $myIds));
+    }
 }
