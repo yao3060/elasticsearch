667008b50dac03574932ce124b15d35e7a88c19c
YangShangPu <yangshangpu@tuguaishou.com>
2021-09-15 16:37:44 +0800

    新增ESSearchWord单元测试

---
+++ src/tests/unit/models/SearchWordTest.php
@@ -0,0 +1,88 @@
+<?php
+namespace tests\unit\models;
+
+use app\components\IpsAuthority;
+use app\models\ES\SearchWord;
+use app\queries\ES\SearchWordSearchQuery;
+use yii\helpers\ArrayHelper;
+
+class SearchWordTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private $http;
+
+    protected function _before()
+    {
+        IpsAuthority::definedAuth(); // 初始化权限变量
+        $this->http = new \GuzzleHttp\Client();
+    }
+    protected function _after()
+    {
+    }
+    public function testSomeFeature()
+    {
+        $items = (new SearchWord())
+            ->search(new SearchWordSearchQuery(
+                keyword:'你好',
+                pageSize:20,
+                type:1,
+            ));
+        /**@var \GuzzleHttp\Psr7\Response $response */
+        $response = $this->http->request(
+            'GET',
+            'https://818ps.com/site/sphinx?1=1&keyword=%E4%BD%A0%E5%A5%BD&type=1&max=6'
+        );
+
+        $content = json_decode($response->getBody()->getContents());
+        $ids = ArrayHelper::getColumn($content, 'id');
+        sort($ids);
+        $myIds = $items['ids'];
+        sort($myIds);
+        $flag = 1;
+        foreach ($ids as $va) {
+            if (in_array($va, $myIds)) {
+                continue;
+            }else {
+                $flag = 0;
+                break;
+            }
+        }
+        $this->assertEquals($flag,1);
+    }
+    public function testSearch()
+    {
+        $items = (new SearchWord())
+            ->search(new SearchWordSearchQuery(
+                keyword:'再见',
+                pageSize:20,
+                type:1,
+            ));
+        /**@var \GuzzleHttp\Psr7\Response $response */
+        $response = $this->http->request(
+            'GET',
+            'https://818ps.com/site/sphinx?1=1&keyword=%E5%86%8D%E8%A7%81&type=1&max=6'
+        );
+
+        $content = json_decode($response->getBody()->getContents());
+        $ids = ArrayHelper::getColumn($content, 'id');
+        sort($ids);
+        $myIds = $items['ids'];
+        sort($myIds);
+        $flag = 1;
+        foreach ($ids as $va) {
+            if (in_array($va, $myIds)) {
+                continue;
+            }else {
+                $flag = 0;
+                break;
+            }
+        }
+        $this->assertEquals($flag,1);
+    }
+}
