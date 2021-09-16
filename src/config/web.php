778f2c5cd5a4a461ee2eedcd4036531c02737e7f
YYY RMBP <you3060@163.com>
2021-09-16 18:29:27 +0800

    add codemix/yii2-streamlog

--- src/config/web.php
+++ src/config/web.php
@@ -76,8 +76,20 @@ $config = [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
                 [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'info', 'warning'],
+                    'class' => \yii\log\FileTarget::class,
+                    'levels' => [],
+                ],
+                [
+                    'class' => \codemix\streamlog\Target::class,
+                    'url' => 'php://stdout',
+                    'levels' => ['info', 'trace'],
+                    'logVars' => [],
+                ],
+                [
+                    'class' => \codemix\streamlog\Target::class,
+                    'url' => 'php://stderr',
+                    'levels' => ['error', 'warning'],
+                    'logVars' => [],
                 ],
             ],
         ],
