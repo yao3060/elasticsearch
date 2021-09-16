819ff7602b01e29785bb10b41f37c712e99ef784
hulifeng <9265959@qq.com>
2021-09-16 13:49:27 +0800

    add lottie video and add lottie video test unit and modify unit-doc example

---
+++ src/models/ES/LottieVideo.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace app\models\ES;
+
+
+use app\components\Tools;
+use app\interfaces\ES\QueryBuilderInterface;
+use yii\base\Exception;
+
+class LottieVideo extends BaseModel
+{
+    public static $redisDb = 8;
+
+    public static function index()
+    {
+        return 'video_lottie';
+    }
+
+    public static function type()
+    {
+        return 'list';
+    }
+
+    public function attributes()
+    {
+        return ['id', 'title', 'create_date', 'pr', 'width', 'height', 'class_id', 'description'];
+    }
+
+    public function search(QueryBuilderInterface $query) :array
+    {
+        $return = Tools::getRedis(self::$redisDb, $query->getRedisKey());
+
+        if (!$return || Tools::isReturnSource() || $query->prep) {
+            unset($return);
+
+            $return['hit'] = 0;
+            $return['ids'] = [];
+            $return['score'] = [];
+
+            try {
+
+                $info = self::find()
+                    ->source(['id'])
+                    ->query($query->query())
+                    ->orderBy($query->sort)
+                    ->offset($query->offset)
+                    ->limit($query->pageSize)
+                    ->createCommand()
+                    ->search([], ['track_scores' => true])['hits'];
+
+            } catch (\exception $e) {
+
+                throw new Exception($e->getMessage());
+
+            }
+
+            if (isset($info['hits']) && sizeof($info['hits'])) {
+
+                $total = $info['total'] ?? 0;
+
+                $return['hit'] = $total > 10000 ? 10000 : $total;
+                foreach ($info['hits'] as $value) {
+                    $return['ids'][] = $value['_id'];
+                    $return['score'][$value['_id']] = $value['sort'][0];
+                }
+
+            }
+
+//            Tools::setRedis(self::$redisDb, $query->getRedisKey(), $return, 86400);
+        }
+
+        return $return;
+    }
+}
