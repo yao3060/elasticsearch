46ac7623eb4b5dc1c806178e61d2c5c72dd0a795
YYY RMBP <you3060@163.com>
2021-09-22 11:15:59 +0800

    update test config

--- src/web/index.php
+++ src/web/index.php
@@ -1,10 +1,5 @@
 <?php
 
-if (getenv('APP_DEBUG')) {
-    defined('YII_DEBUG') or define('YII_DEBUG', true);
-    defined('YII_ENV') or define('YII_ENV', 'dev');
-}
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
