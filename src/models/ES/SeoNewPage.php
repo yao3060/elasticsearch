cd08fca53287221fdc6d02fa39f9efa89e64f093
YangShangPu <yangshangpu@tuguaishou.com>
2021-09-27 15:52:07 +0800

    修复SeoNewPage查询bug

--- src/models/ES/SeoNewPage.php
+++ src/models/ES/SeoNewPage.php
@@ -39,38 +39,31 @@ class SeoNewPage extends BaseModel
         $return = Tools::getRedis(self::$redisDb, $redisKey);
 
         if (!$return || Tools::isReturnSource()) {
-
             $return['hit'] = 0;
             $return['ids'] = [];
             $return['score'] = [];
 
             try {
-
                 $info = self::find()
                     ->source(['id', '_keyword'])
                     ->query($query->query())
                     ->limit($query->pageSize)
                     ->createCommand()
                     ->search([], ['track_scores' => true])['hits'];
-
             } catch (\exception $e) {
-
                 throw new Exception($e->getMessage());
-
             }
 
             $total = $info['total'] ?? [];
 
             if (isset($info['hits']) && $info['hits'] && $total > 0) {
-
-                foreach ($info['hits'] as $k=>$v) {
+                foreach ($info['hits'] as $k => $v) {
                     $return[$k]['id'] = $v['_source']['id'];
                     $return[$k]['keyword'] = $v['_source']['_keyword'];
                 }
             }
 
             Tools::setRedis(self::$redisDb, $redisKey, $return, 86400 * 30);
-
         }
 
         return $return;
