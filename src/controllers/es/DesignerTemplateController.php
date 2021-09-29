880508e0ad9454dcc5edda1a0739814b54bf9109
hulifeng <9265959@qq.com>
2021-09-29 16:38:32 +0800

    designer template api doc

--- src/controllers/es/DesignerTemplateController.php
+++ src/controllers/es/DesignerTemplateController.php
@@ -13,6 +13,61 @@ use yii\base\UnknownPropertyException;
 class DesignerTemplateController extends BaseController
 {
 
+    /**
+     * @api {get} /v1/designer-templates Get DesignerTemplate
+     * @apiName GetDesignerTemplate
+     * @apiGroup DesignerTemplate
+     *
+     * @apiParam (请求参数) {String} keyword 搜索关键词
+     * @apiParam (请求参数) {Number} [page] 页码
+     * @apiParam (请求参数) {Number} [kid1] 一级版式
+     * @apiParam (请求参数) {Number} [kid2] 二级版式
+     * @apiParam (请求参数) {String} [sort_type] 排序  byyesday：昨日热门 ；bymonth：热门下载；bytime：最新上传
+     * @apiParam (请求参数) {Number} [tag_id] 风格
+     * @apiParam (请求参数) {Boolean} [is_zb] 是否可商用   >= 1 可商用
+     * @apiParam (请求参数) {Number} [page_size] 每页条数
+     * @apiParam (请求参数) {Number} [ratio] 版式  null：全部 1：横图；2：竖图；0：方图
+     * @apiParam (请求参数) {String[]} [class_id] 分类
+     * @apiParam (请求参数) {Number} [update] 1：强制回源
+     * @apiParam (请求参数) {Number} [size] 模板大小（已下线）
+     * @apiParam (请求参数) {Number} [fuzzy] 1：关键词（有交集）就会出现   但会降低性能  实测 3个词速度降低50%  8个词速度降低87.5%
+     * @apiParam (请求参数) {string[]} [template_type] 模板类型 1.普通模板；2.GIF模板 3.ppt模板 4.视频模板 5.H5模板 & 长页H5
+     * @apiParam (请求参数) {Object[]} [templ_info] 模板属性（精品 or 非精品，只在设计师使用）
+     * @apiParam (请求参数) {String[]} [color] 颜色搜索
+     * @apiParam (请求参数) {Number} [use] 强制匹配类型(已下线）
+     *
+     * @apiSuccess (应答字段) {String} code 返回状态码
+     * @apiSuccess (应答字段) {String} message 返回消息
+     * @apiSuccess (应答字段) {Object[]} data 返回数据
+     * @apiSuccess (应答字段) {String} data.hit 命中数
+     * @apiSuccess (应答字段) {String[]} data.ids 模板id集合
+     * @apiSuccess (应答字段) {String[]} data.score 计算分数
+     * @apiSuccess (应答字段) {Number} data.total 模板数量
+     *
+     * @apiSuccessExample {json} 应答事例
+     *  {
+     *     "code": "design_template_index",
+     *     "message": "DesignTemplateIndex",
+     *     "data": {
+     *          "hit": 10000,
+     *          "ids": [
+     *               "3120406",
+     *               "3352082",
+     *               "3350802",
+     *               "3376118",
+     *               "3313747"
+     *         ],
+     *         "score": {
+     *               "3120406": 5889,
+     *               "3352082": 5865,
+     *              "3350802": 5774,
+     *              "3376118": 5668,
+     *              "3313747": 5647
+     *         },
+     *         "total": 53726
+     *      }
+     *  }
+     */
     public function actionIndex(Request $request)
     {
         try {
