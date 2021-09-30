45ac093fa5b458a34a64752828f84874fef6eab9
hulifeng <9265959@qq.com>
2021-09-30 14:48:05 +0800

    seo search word asset controller api doc

--- src/controllers/es/SeoSearchWordAssetController.php
+++ src/controllers/es/SeoSearchWordAssetController.php
@@ -20,9 +20,9 @@ use yii\web\Request;
 class SeoSearchWordAssetController extends BaseController
 {
     /**
-     * @api {get} /v1/seo/keyword-assets Seo keyword Asset
-     * @apiName GetSeoKeywordAsset
-     * @apiGroup SeoKeywordAsset
+     * @api {get} /v1/seo/keyword-assets Seo Search Word Asset
+     * @apiName GetSeoWordAsset
+     * @apiGroup SeoWordAsset
      *
      * @apiParam (请求参数) {String} keyword 搜索关键词
      * @apiParam (请求参数) {Number} [page] 页码
@@ -32,32 +32,32 @@ class SeoSearchWordAssetController extends BaseController
      * @apiSuccess (应答字段) {String} code 返回状态码
      * @apiSuccess (应答字段) {String} message 返回消息
      * @apiSuccess (应答字段) {Object[]} data 返回数据
-     * @apiSuccess (应答字段) {String} data.hit 命中数
-     * @apiSuccess (应答字段) {String[]} data.ids 模板id集合
-     * @apiSuccess (应答字段) {String[]} data.score 计算分数
-     * @apiSuccess (应答字段) {Number} data.total 模板数量
+     * @apiSuccess (应答字段) {String[]} data.list
+     * @apiSuccess (应答字段) {Number} data.total 数据总量
      *
      * @apiSuccessExample {json} 应答事例
      *  {
-     *     "code": "design_template_index",
-     *     "message": "DesignTemplateIndex",
+     *     "code": "get_seo_search_word_asset_list",
+     *     "message": "Seo Search Word Asset List",
      *     "data": {
-     *          "hit": 10000,
-     *          "ids": [
-     *               "3120406",
-     *               "3352082",
-     *               "3350802",
-     *               "3376118",
-     *               "3313747"
-     *         ],
-     *         "score": {
-     *               "3120406": 5889,
-     *               "3352082": 5865,
-     *              "3350802": 5774,
-     *              "3376118": 5668,
-     *              "3313747": 5647
-     *         },
-     *         "total": 53726
+     *          "total": 273,
+     *          "list": [
+     *              {
+     *                    "id": 407238,
+     *                    "keyword": "",
+     *                    "pinyin": "234fdd84877d2f7501878b44603915d0",
+     *                    "weight": 3
+     *              },
+     *              {
+     *                    "id": 441117,
+     *                    "keyword": "",
+     *                    "pinyin": "logolvyou",
+     *                    "weight": 0
+     *              }
+     *
+     *              ....
+     *
+     *         ]
      *      }
      *  }
      */
@@ -78,7 +78,7 @@ class SeoSearchWordAssetController extends BaseController
                         $data['page_size'] ?? 40,
                         $data['type'] ?? 1,
                     ));
-                $response = new Response('get_seo_search_word_asset_list', 'SeoSearchWordAssetList', $data);
+                $response = new Response('get_seo_search_word_asset_list', 'Seo Search Word Asset List', $data);
             }
         } catch (UnknownPropertyException $e) {
             $response = new Response(
