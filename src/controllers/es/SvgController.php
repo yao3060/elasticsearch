aa81ce760a1ce4c7115e94ae45eef665c97df553
Yao.Yingying <you3060@163.com>
2021-10-09 10:25:43 +0800

    fix svg error

--- src/controllers/es/SvgController.php
+++ src/controllers/es/SvgController.php
@@ -32,22 +32,14 @@ class SvgController extends BaseController
      */
     public function actionIndex(Request $request)
     {
-
         try {
-            $model = DynamicModel::validateData($request->get(), [
-                ['keyword', 'string']
-            ]);
-            if ($model->hasErrors()) {
-                $response = new Response('unprocessable_entity', 'Unprocessable Entity', $model->errors, 422);
-            } else {
-                $items = (new Svg)->search(new SvgSearchQuery(
-                    keyword: $request->get('keyword', 0),
-                    kid2: $request->get('kid2', 0),
-                    page: $request->get('page', 1),
-                    pageSize: $request->get('page_size', 40)
-                ));
-                $response = new Response('get_svg_list', 'Get Svg List', $items);
-            }
+            $items = (new Svg)->search(new SvgSearchQuery(
+                keyword: $request->get('keyword', 0),
+                kid2: $request->get('kid2', 0),
+                page: $request->get('page', 1),
+                pageSize: $request->get('page_size', 40)
+            ));
+            $response = new Response('get_svg_list', 'Get Svg List', $items);
         } catch (UnknownPropertyException $e) {
             $response = new Response(
                 StringHelper::snake($e->getName()),
